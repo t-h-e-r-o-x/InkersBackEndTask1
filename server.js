edbdd3c8e489eb4b7ed1dcc042d0c28b64eff788
t-h-e-r-o-x <31180828+t-h-e-r-o-x@users.noreply.github.com>
2019-03-22 23:55:42 +0530

    Changing Port

--- server.js
+++ server.js
@@ -63,6 +63,6 @@ app.get('/piedata', (req,res) => {
   })
 })
 
-app.listen(3000 , () => {
-  console.log("App is running on port 3000");
+app.listen(process.env.PORT || 3000 , () => {
+  console.log(`App is running on port ${process.env.PORT}`);
 });
